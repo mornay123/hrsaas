c45688fffa92db5b4caec5af84821c23b0bfef0a
杨欣悦 <11490425+yang-mornay@user.noreply.gitee.com>
2022-09-27 14:58:46 +0800

    'feat:描述信息'

---
+++ src/router/modules/salarys.js
@@ -0,0 +1,15 @@
+import Layout from '@/layout'
+export default {
+  path: '/salarys',
+  component: Layout,
+  children: [{
+    name: 'salarys',
+    path: '',
+    component: () => import('@/views/salarys'),
+    meta: {
+      title: '工资',
+      icon: 'money'
+    }
+  }]
+
+}
